159e5521cda30779aa52c7af62f0ab6219aa88b1
degarashi <slice013@gmail.com>
2018-09-28 19:10:58 +0900

    [update]翻訳ファイルの定義(japanese)

---
+++ translation/sprinkler_ja.ts
@@ -0,0 +1,253 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="en">
+<context>
+    <name>DirList</name>
+    <message>
+        <location filename="../src/dirlist.ui" line="14"/>
+        <source>ImageSource</source>
+        <translation>画像ソース</translation>
+    </message>
+    <message>
+        <location filename="../src/dirlist.ui" line="44"/>
+        <source>Add</source>
+        <translation>追加</translation>
+    </message>
+    <message>
+        <location filename="../src/dirlist.ui" line="55"/>
+        <source>Remove</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="../src/dirlist.ui" line="81"/>
+        <source>AddDir</source>
+        <translation>追加</translation>
+    </message>
+    <message>
+        <location filename="../src/dirlist.ui" line="86"/>
+        <source>RemDir</source>
+        <translation>削除</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>メインウィンドウ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="36"/>
+        <location filename="../src/mainwindow.ui" line="261"/>
+        <source>ImageSource</source>
+        <translation>画像ソース</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="58"/>
+        <location filename="../src/mainwindow.ui" line="288"/>
+        <source>WindowWatch</source>
+        <translation>ウィンドウ監視</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="71"/>
+        <location filename="../src/mainwindow.ui" line="272"/>
+        <source>StayOnTop</source>
+        <translation>常に最前面に表示</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="100"/>
+        <source>Controls</source>
+        <translation>コントロール</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="131"/>
+        <source>Reset</source>
+        <comment>Sprinkle</comment>
+        <translation>リセット</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="138"/>
+        <source>Sprinkle</source>
+        <translation>画像を散布</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="158"/>
+        <source>Re-Sprinkle</source>
+        <translation>(前回の画像セットを)再度散布</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="181"/>
+        <source>Keep</source>
+        <translation>キープ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="188"/>
+        <source>Remove</source>
+        <comment>Keep</comment>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="204"/>
+        <source>ImageCollect-Constraint</source>
+        <translation>画像収集条件</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="222"/>
+        <source>PreferOriginal</source>
+        <translation>オリジナルサイズを優先</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="242"/>
+        <source>Menu</source>
+        <translation>メニュー</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="264"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="277"/>
+        <source>Quit</source>
+        <translation>終了</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="280"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="291"/>
+        <source>Ctrl+D</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>WatchList</name>
+    <message>
+        <location filename="../src/watchlist.ui" line="14"/>
+        <source>WindowWatch</source>
+        <translation>ウィンドウ監視</translation>
+    </message>
+    <message>
+        <location filename="../src/watchlist.ui" line="41"/>
+        <source>Remove</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="../src/watchlist.ui" line="54"/>
+        <source>RemoveWatch</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="../src/watchlist.ui" line="57"/>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>dg::CurIndicator</name>
+    <message>
+        <location filename="../src/cur_indicator.cpp" line="5"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; images (out of &lt;b&gt;%2&lt;/b&gt;) were displayed</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt;枚の画像を表示済み(&lt;b&gt;%2&lt;/b&gt;枚中)</translation>
+    </message>
+</context>
+<context>
+    <name>dg::DirList</name>
+    <message>
+        <location filename="../src/dirlist.cpp" line="15"/>
+        <source>Select Image Directory</source>
+        <translation>画像フォルダを選択して下さい</translation>
+    </message>
+</context>
+<context>
+    <name>dg::GLabel</name>
+    <message>
+        <location filename="../src/glabel.cpp" line="39"/>
+        <source>Keep</source>
+        <translation>キープ</translation>
+    </message>
+    <message>
+        <location filename="../src/glabel.cpp" line="40"/>
+        <source>k</source>
+        <comment>Keep</comment>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>dg::MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="114"/>
+        <source>Error occurred</source>
+        <translation>エラー発生</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="133"/>
+        <source>Max</source>
+        <translation>最大</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="140"/>
+        <source>Min</source>
+        <translation>最小</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="147"/>
+        <source>Samp</source>
+        <translation>サンプル倍率</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="244"/>
+        <source>Path</source>
+        <translation>パス</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="245"/>
+        <source>ImageCount</source>
+        <translation>画像カウント</translation>
+    </message>
+</context>
+<context>
+    <name>dg::Watcher</name>
+    <message>
+        <location filename="../src/dep_linux/watcher.cpp" line="34"/>
+        <source>Add</source>
+        <translation>追加</translation>
+    </message>
+    <message>
+        <location filename="../src/dep_linux/watcher.cpp" line="70"/>
+        <source>unable to connect to display</source>
+        <translation>ディスプレイへの接続に失敗</translation>
+    </message>
+    <message>
+        <location filename="../src/dep_linux/watcher.cpp" line="191"/>
+        <source>(noname)</source>
+        <translation>(名無し)</translation>
+    </message>
+    <message>
+        <location filename="../src/dep_linux/watcher.cpp" line="212"/>
+        <source>error in select()</source>
+        <translation>select()関数でエラー発生</translation>
+    </message>
+    <message>
+        <location filename="../src/dep_linux/watcher.cpp" line="261"/>
+        <location filename="../src/dep_linux/watcher.cpp" line="282"/>
+        <source>Not running thread</source>
+        <translation>スレッドがまだスタートしていません</translation>
+    </message>
+    <message>
+        <location filename="../src/dep_linux/watcher.cpp" line="291"/>
+        <source>Already running</source>
+        <translation>スレッドは既にスタート済み</translation>
+    </message>
+</context>
+<context>
+    <name>rectwindow</name>
+    <message>
+        <location filename="../src/rectwindow.ui" line="23"/>
+        <source>QuantizedView</source>
+        <translation>マス目表示</translation>
+    </message>
+</context>
+</TS>
