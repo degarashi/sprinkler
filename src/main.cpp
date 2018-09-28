26ab27d9e2e5398f1641e72509cf1af0e74b5aa1
degarashi <slice013@gmail.com>
2018-09-28 20:13:58 +0900

    [update]翻訳ファイルの読み込み&適用

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.hpp"
 #include <QApplication>
 #include "dirlist.hpp"
+#include <QTranslator>
+#include <QTextCodec>
+#include <QLibraryInfo>
+#include <QDebug>
 
 int main(int argc, char *argv[]) {
 	// for XmbTextPropertyToTextList
@@ -10,6 +14,19 @@ int main(int argc, char *argv[]) {
 	QCoreApplication::setOrganizationName("DegarashiLab");
 	QCoreApplication::setOrganizationDomain("Degarashi.deg");
 	QCoreApplication::setApplicationName("sprinkler");
+
+	// Install Translation
+	QTranslator tr;
+	{
+		QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf8"));
+		QString loc = QLocale::system().name();
+
+		tr.load("qtbase_" + loc,
+				QLibraryInfo::location(QLibraryInfo::TranslationsPath)
+			   );
+		tr.load(QString("sprinkler_") + loc, QApplication::applicationDirPath());
+		a.installTranslator(&tr);
+	}
 
 	dg::MainWindow mw;
 	mw.show();
