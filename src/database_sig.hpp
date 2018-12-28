a229b70596a9729c392b37d736030d05431d6f56
degarashi <slice013@gmail.com>
2018-12-28 19:30:05 +0900

    [add]DatabaseSignal: beginResetLink(), endResetLink()を定義

--- src/database_sig.hpp
+++ src/database_sig.hpp
@@ -11,10 +11,12 @@ namespace dg {
 			void beginResetDir();
 			void beginResetImage();
 			void beginResetTag();
+			void beginResetLink();
 
 		 	void endResetDir();
 			void endResetImage();
 			void endResetTag();
+			void endResetLink();
 		public:
 			explicit DatabaseSignal(QObject* parent=nullptr);
 	};
