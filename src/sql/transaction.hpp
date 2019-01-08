1635d44ca553a16e789cf7fb41d610ec618d1c5d
degarashi <slice013@gmail.com>
2019-01-08 15:02:48 +0900

    [update]:sql::Transaction(): ネスト対応

--- src/sql/transaction.hpp
+++ src/sql/transaction.hpp
@@ -5,15 +5,32 @@
 #include <exception>
 
 namespace dg::sql {
+	// ネスト対応
 	template <class Proc>
 	void Transaction(QSqlDatabase db, Proc&& proc) {
-		if(!db.transaction())
-			ThrowError(db, QStringLiteral("can't begin transaction"));
+		// セーブポイント名を決定するカウンタ
+		// (とりあえず現時点ではシングルスレッドを想定)
+		static int s_count = 0;
+		// 一度Transactionを開始してみてFalseが返るようならネストしていると判断
+		const bool b = db.transaction();
+		if(!b) {
+			// セーブポイントを定義
+			sql::Query(QString("SAVEPOINT Nest_%1").arg(s_count++));
+		}
 		try {
 			proc();
-			db.commit();
+			if(b)
+				db.commit();
+			else
+				--s_count;
 		} catch(const std::exception& e) {
-			db.rollback();
+			if(b)
+				db.rollback();
+			else {
+				// セーブポイントを復元
+				Q_ASSERT(s_count > 0);
+				sql::Query(QString("ROLLBACK TO Nest_%1").arg(--s_count));
+			}
 			throw;
 		}
 	}
